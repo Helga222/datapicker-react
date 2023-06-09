0f9996b7d087f986ba293f279eef72952eb32132
Helga222 <barykina.o@yandex.ru>
2023-06-10 01:12:08 +0300

    new

--- src/Components/DataPicker/DataPicker.tsx
+++ src/Components/DataPicker/DataPicker.tsx
@@ -8,8 +8,10 @@ import Dialog from "@mui/material/Dialog";
 import TextField from "@mui/material/TextField";
 import { FilledInput } from "@mui/material";
 import { TimeString } from "../../types";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { normalize } from "path";
 export const DataPicker = () => {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
   const modalRef = useRef(null);
   const [timeString, setTimeString] = useState({
     since: "за последние",
@@ -18,15 +20,14 @@ export const DataPicker = () => {
   });
 
   const handleClick = (e: any) => {
-    e.stopPropagation();
-    setVisible(!visible);
-    modalRef.onClose();
+    //e.stopPropagation();
+    setVisible(true);
   };
 
   const handleApplyDate = (time: TimeString) => {
     setTimeString(time);
+    setVisible(false);
   };
-
 
   return (
     <div className={styles.wrapper}>
@@ -40,29 +41,52 @@ export const DataPicker = () => {
             <KeyboardArrowDownIcon />
           </div>
 
-
-
           <div
             className={`${styles.dp__popup} ${
               visible ? styles.dp__popup_visible : styles.dp__popup_visible
             }`}
           >
-          <Dialog open={visible} ref={modalRef} onClick={(e:any)=>e.stopPropagation()}> 
-            <Menu onHandleClick={handleApplyDate} />
-          </Dialog>
+            <Dialog open={visible} ref={modalRef}>
+              <Menu onHandleClick={handleApplyDate} />
+            </Dialog>
           </div>
         </button>
         <div className={`${styles.dp__item} ${styles.dp__date}`}>
-          <FilledInput
+          {/*<FilledInput
             readOnly
             disableUnderline={true}
             className={styles.dp__textInput}
+            sx={{ input: { cursor: 'pointer' } }}
             fullWidth
             id="fullWidth"
             value={`${timeString.since} ${timeString.time} ${timeString.unit}`}
             hiddenLabel
             size="small"
-          />
+          />*/}
+
+          <div className={styles.dp__date__content}>
+            <TextField
+              size="small"
+              fullWidth
+              inputProps={{ style: { textAlign: "right" } }}
+              value={`${timeString.since} ${timeString.time} ${timeString.unit}`}
+              hiddenLabel
+              variant="filled"
+            />
+          </div>
+          <div className={styles.dp__icon}>
+
+          <ArrowForwardIcon fontSize="medium"/>
+          </div>
+          <div className={styles.dp__date__content}>
+            <TextField
+              size="small"
+              fullWidth
+              value="now"
+              hiddenLabel
+              variant="filled"
+            />
+          </div>
         </div>
         <button className={`${styles.dp__item} ${styles.dp__refreshButton}`}>
           3
